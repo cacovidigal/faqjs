db9c83e062171ad132b20b9d0363c01a16bc46e2
cacovidigal <marcosmsvidigal@gmail.com>
2023-06-26 17:00:53 -0300

    faq com js

---
+++ respostas.js
@@ -0,0 +1,28 @@
+function exibirResposta_1() {
+  var resposta_1 = "<p> You can invite up to 2 additional users on the Free plan. There is no limit on team members for the Premium plan.</p>";
+    document.getElementById("resposta_1").innerHTML =  `<div style="font-size: 16px; font-weight: 700; margin-right: 15px; text-align: center;  margin-left: 10px; color: #333;">${resposta_1}</div>` ;
+    
+}
+
+function exibirResposta_2() {
+    var resposta_2 = "<p> No more than 2GB. All files in your account must fit your allotted storage space.</p>";
+      document.getElementById("resposta_2").innerHTML = `<div style="font-size: 16px; font-weight: 700; margin-right: 15px; text-align: center;  margin-left: 10px; color: #333;">${resposta_2}</div>` ;
+  }
+
+function exibirResposta_3() {
+    var resposta_3 = "<p>Forgot password” from the login page or “Change password” from your profile page. A reset link will be emailed to you.</p>";
+      document.getElementById("resposta_3").innerHTML = `<div style="font-size: 16px; font-weight: 700; margin-right: 15px; text-align: center;  margin-left: 10px; color: #333;">${resposta_3}</div>` ;
+  }
+
+function exibirResposta_4() {
+    var resposta_4 = "<p>Yes! Send us a message and we will process your request no questions asked.</p>";
+      document.getElementById("resposta_4").innerHTML = `<div style="font-size: 16px; font-weight: 700; margin-right: 15px; text-align: center;  margin-left: 10px; color: #333;">${resposta_4}</div>` ;
+  }
+
+function exibirResposta_5() {
+    var resposta_5 = "<p>Chat and email support is available 24/7. Phone lines are open during normal business hours.</p>";
+      document.getElementById("resposta_5").innerHTML = `<div style="font-size: 16px; font-weight: 700; margin-right: 15px; text-align: center;  margin-left: 10px; color: #333;">${resposta_5}</div>` ;
+  }
+
+     
+            
